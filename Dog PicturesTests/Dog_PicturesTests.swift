6cab466ba2a4c97250791ab66084508f6ec01fca
Brian Rodriguez <thatbrod@gmail.com>
2016-04-06 21:04:34 -0400

    Initial Commit

---
+++ Dog PicturesTests/Dog_PicturesTests.swift
@@ -0,0 +1,36 @@
+//
+//  Dog_PicturesTests.swift
+//  Dog PicturesTests
+//
+//  Created by Brian Rodriguez on We/6/4.
+//  Copyright © 2016 Treesun Studios. All rights reserved.
+//
+
+import XCTest
+@testable import Dog_Pictures
+
+class Dog_PicturesTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
